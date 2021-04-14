48dcfeb2aacf0789911635cd5e7bbd4407e81b1e
lolmycode <rcc428@nyu.edu>
2021-04-14 19:30:44 -0400

    magnetic poem done

--- prose/magneticpoem/magnetjs.js
+++ prose/magneticpoem/magnetjs.js
@@ -8,9 +8,27 @@ $(document).ready(function() {
   $("#wo6").draggable();
   $("#wo7").draggable();
   $("#wo9").draggable();
+  $("#wo10").draggable();
 
   $("#r2w1").draggable();
+  $("#r2w2").draggable();
+  $("#r2w3").draggable();
+  $("#r2w4").draggable();
+  $("#r2w5").draggable();
+  $("#r2w6").draggable();
+  $("#r2w7").draggable();
+  $("#r2w8").draggable();
 
+  $("#r3w1").draggable();
+  $("#r3w2").draggable();
+  $("#r3w3").draggable();
+  $("#r3w4").draggable();
+  $("#r3w4").draggable();
+  $("#r3w5").draggable();
+  $("#r3w6").draggable();
+  $("#r3w7").draggable();
+  $("#r3w8").draggable();
+  $("#r3w9").draggable();
   // $("#picthree").click(function(){
   //   $("#styleone").show();
   // });
