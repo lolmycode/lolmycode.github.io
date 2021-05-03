d59d2fec10b4276fc8b3d506ed163b64e43b409b
lolmycode <rcc428@nyu.edu>
2021-05-03 12:06:33 -0400

    try new file

---
+++ magkeywords/magdrag.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+  $("#wo1").draggable();
+  $("#wo2").draggable();
+  $("#wo3").draggable();
+  $("#wo4").draggable();
+  $("#wo5").draggable();
+  $("#wo6").draggable();
+  $("#wo7").draggable();
+  $("#wo9").draggable();
+  $("#wo10").draggable();
+  $("#wo11").draggable();
+
+  $("#r2w1").draggable();
+  $("#r2w2").draggable();
+  $("#r2w3").draggable();
+  $("#r2w4").draggable();
+  $("#r2w5").draggable();
+  $("#r2w6").draggable();
+  $("#r2w7").draggable();
+  $("#r2w8").draggable();
+
+  $("#r3w1").draggable();
+  $("#r3w2").draggable();
+  $("#r3w3").draggable();
+  $("#r3w4").draggable();
+  $("#r3w5").draggable();
+  $("#r3w6").draggable();
+  $("#r3w7").draggable();
+  $("#r3w8").draggable();
+  $("#r3w9").draggable();
+  // $("#picthree").click(function(){
+  //   $("#styleone").show();
+  // });
+
+  $("#microtext").hover(function(){
+    $("#styleone").show();
+  });
+
+  $("#bluecopy").click(function(){
+    $("#animatedcap").show();
+  });
+
+  $("#typeinbox").click(function(){
+    $(this).hide();
+  });
+
+})
